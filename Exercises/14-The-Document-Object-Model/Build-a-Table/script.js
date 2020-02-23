fd55d01dea130eb555358c6443f88816df747d43
Pablo Gracia <pablo.graciag@gmail.com>
2020-02-23 12:32:30 +0100

    Initialized Build a Table exercise (not sure on how to do it with <th>. Nead to read more.

---
+++ Exercises/14-The-Document-Object-Model/Build-a-Table/script.js
@@ -0,0 +1,13 @@
+const mountain_data = [
+  { name: "Kilimanjaro", height: 5895, place: "Tanzania" },
+  { name: "Aneto", height: 3404, place: "Spain" },
+  { name: "Everest", height: 8848, place: "Tibet" }
+];
+
+function main(mountain_data) {
+  const app = document.getElementsByClassName("app");
+
+  const table = document.createElement("TABLE");
+
+  const header = table.createTHead();
+}
